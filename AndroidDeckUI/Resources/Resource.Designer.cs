7beea0bd3bf6f17c1fe56cbc2a3b50ca9e40de25
awroe <github@andywroe.co.uk>
2013-12-04 11:31:54 +0000

    Additional button styling, support for new CurrentSlide class

--- AndroidDeckUI/Resources/Resource.Designer.cs
+++ AndroidDeckUI/Resources/Resource.Designer.cs
@@ -45,10 +45,46 @@ namespace Oxfordcc.DevCamp2013.AndroidDeckUI
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int down = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int monoandroidsplash = 2130837505;
+			public const int down_button = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int down_on = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int Icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int left = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int left_button = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int left_on = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int monoandroidsplash = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int right = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int right_button = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int right_on = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int up = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int up_button = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int up_on = 2130837517;
 			
 			static Drawable()
 			{
